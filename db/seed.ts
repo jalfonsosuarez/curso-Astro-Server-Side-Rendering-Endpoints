997e20d8305ac65c84d104d06f9d4c1e49086990
= <=>
2024-07-30 18:32:13 +0200

    AntroDB in not available in tis moment

--- db/seed.ts
+++ db/seed.ts
@@ -3,7 +3,7 @@ import { Clients, db } from "astro:db";
 // https://astro.build/db/seed
 export default async function seed() {
   await db.insert(Clients).values([
-    { id: 1, name: "Jose", age: 59, isActive: true },
-    { id: 2, name: "Patricia", age: 53, isActive: true },
+    { name: "Jose", age: 59, isActive: true },
+    { name: "Patricia", age: 53, isActive: true },
   ]);
 }
